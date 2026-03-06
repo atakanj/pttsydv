f7a2bd0e86b9be1c1d2f97e5ea7e7dd69feee7af
atakanj <102961392+atakanj@users.noreply.github.com>
2026-03-06 09:59:55 +0300

    Add files via upload

---
+++ kullanicilar.js
@@ -0,0 +1,5 @@
+var KULLANICILAR=[
+  {kullanici:'atakan',sifre:'123456',ayarlar:{kurum_kodu:'555',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}},
+  {kullanici:'irem',sifre:'654321',ayarlar:{kurum_kodu:'444',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}},
+  {kullanici:'ahmet',sifre:'123123',ayarlar:{kurum_kodu:'6767',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}}
+];
