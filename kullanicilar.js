d6e026f3a6f2a91ed879dcb47c5c73c4f80d77e3
atakanj <102961392+atakanj@users.noreply.github.com>
2026-03-06 10:18:21 +0300

    Update kullanicilar.js

--- kullanicilar.js
+++ kullanicilar.js
@@ -1,5 +1,6 @@
 var KULLANICILAR=[
   {kullanici:'atakan',sifre:'123456',ayarlar:{kurum_kodu:'555',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}},
   {kullanici:'irem',sifre:'654321',ayarlar:{kurum_kodu:'444',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}},
-  {kullanici:'ahmet',sifre:'123123',ayarlar:{kurum_kodu:'6767',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}}
+  {kullanici:'ahmet',sifre:'123123',ayarlar:{kurum_kodu:'6767',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}},
+  {kullanici:'mehmet',sifre:'741852',ayarlar:{kurum_kodu:'1000',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}}
 ];
