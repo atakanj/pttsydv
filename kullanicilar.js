a1250e2f2179ec92ce44d0ba5a9def6cb7129765
atakanj <102961392+atakanj@users.noreply.github.com>
2026-03-06 15:08:04 +0300

    Update kullanicilar.js

--- kullanicilar.js
+++ kullanicilar.js
@@ -1,6 +1,63 @@
 var KULLANICILAR=[
-  {kullanici:'atakan',salt:'4f2939410f8e6bbd396a40b4b76af678',hash:'9d5f60f462c18c398ea2844ee9312e6b065b545a35b475365e6cc9afacc65eb4',ayarlar:{kurum_kodu:'555',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}},
-  {kullanici:'irem',salt:'03adeb592dc1e6963870d89c75cf8a30',hash:'6ec1e8e436336c698c5766f4a4a5d29d3fc6e6ce6dc11857358143c435472fb4',ayarlar:{kurum_kodu:'444',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}},
-  {kullanici:'ahmet',salt:'595fe21bd43d1bcf951c5f15e728cc3a',hash:'3293086a33aa90c13ebcd1ac51f5c62a03e579caceb2c0957c8bae7688817f6e',ayarlar:{kurum_kodu:'6767',merkez_no:'08813',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}},
-  {kullanici:'admin',salt:'23f9f2f203ced65d7122edd8b4d7846f',hash:'c0eacc1e5aef1b676cde8f1952b540300eb1087fc24f214644bb623fd9c37d75',rol:'admin',ayarlar:{kurum_kodu:'',merkez_no:'',dosya_basi:'B',ili:'ISTANBUL',sube_no:'00',ilce:'SARIYER',sydv_hesap:'00000000',odeme_turu:'M'}}
+  {
+    "kullanici": "atakan",
+    "salt": "4f2939410f8e6bbd396a40b4b76af678",
+    "hash": "9d5f60f462c18c398ea2844ee9312e6b065b545a35b475365e6cc9afacc65eb4",
+    "ayarlar": {
+      "kurum_kodu": "555",
+      "merkez_no": "08813",
+      "dosya_basi": "B",
+      "ili": "ISTANBUL",
+      "sube_no": "00",
+      "ilce": "SARIYER",
+      "sydv_hesap": "00000000",
+      "odeme_turu": "M"
+    }
+  },
+  {
+    "kullanici": "irem",
+    "salt": "bc22ecc5bd722dfd4ce2f1dd11028ce3",
+    "hash": "3d94a76afa52e4ecb223ed3621b87171197d9b72f2b2dc0575629e5ff168f712",
+    "ayarlar": {
+      "kurum_kodu": "444",
+      "merkez_no": "08813",
+      "dosya_basi": "B",
+      "ili": "ISTANBUL",
+      "sube_no": "00",
+      "ilce": "SARIYER",
+      "sydv_hesap": "00000000",
+      "odeme_turu": "M"
+    }
+  },
+  {
+    "kullanici": "ahmet",
+    "salt": "595fe21bd43d1bcf951c5f15e728cc3a",
+    "hash": "3293086a33aa90c13ebcd1ac51f5c62a03e579caceb2c0957c8bae7688817f6e",
+    "ayarlar": {
+      "kurum_kodu": "6767",
+      "merkez_no": "08813",
+      "dosya_basi": "B",
+      "ili": "ISTANBUL",
+      "sube_no": "00",
+      "ilce": "SARIYER",
+      "sydv_hesap": "00000000",
+      "odeme_turu": "M"
+    }
+  },
+  {
+    "kullanici": "admin",
+    "salt": "23f9f2f203ced65d7122edd8b4d7846f",
+    "hash": "c0eacc1e5aef1b676cde8f1952b540300eb1087fc24f214644bb623fd9c37d75",
+    "rol": "admin",
+    "ayarlar": {
+      "kurum_kodu": "",
+      "merkez_no": "",
+      "dosya_basi": "B",
+      "ili": "ISTANBUL",
+      "sube_no": "00",
+      "ilce": "SARIYER",
+      "sydv_hesap": "00000000",
+      "odeme_turu": "M"
+    }
+  }
 ];
